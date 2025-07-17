9885a6dfb68dcd5e68d654fb2f1422040aed2992
oxygenn <119449416+oxydetoxy@users.noreply.github.com>
2025-07-17 21:20:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   const [screenImage, setScreenImage] = useState(null);
 
   const connect = () => {
-    const socket = new WebSocket('ws://localhost:8080');
+      const socket = new WebSocket('ws://108.181.194.151:8080');
     socket.onopen = () => {
       socket.send(JSON.stringify({ type: 'viewer-join', sessionId }));
       setStatus('Connecting...');
